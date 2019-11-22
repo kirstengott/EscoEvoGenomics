040a7fd3454d35c1cc56719675d3b86405907b21
Kirsten Leigh Gotting <k.gotting@gmail.com>
2019-11-22 15:57:14 -0600

    updating genome size estimations

--- scripts/genome_size_eval.R
+++ scripts/genome_size_eval.R
@@ -1,29 +1,46 @@
+## Command to make the genome scope summary files parsable
+## for i in `ls */summary.txt`; do cat $i | sed -e "s/,//g" | sed -e "s/ //" | sed -e "s/ //" | sed -e "s/bp//g" | sed -e "s/%//g">${i%.txt}_fix.txt; done
+
 library('tidyverse')
 
 
-read_stats <- read_delim('tables/fastp_stats.txt', col_names = c('genome', 'stat', 'value'), delim = ' ')
+read_stats <- read_delim('tables/fastp_stats.txt', 
+                         col_names = c('file', 'stat', 'value'), 
+                         delim = ' ') %>%
+  group_by(file) %>%
+  mutate(genome = strsplit(file, "_")[[1]][1]) %>%
+  ungroup()
 
-read_stats <- read_stats %>% filter(stat == 'total_bases') %>% 
+read_coverage <- read_stats %>% 
+  filter(stat == 'total_bases') %>% 
   group_by(genome) %>%
   summarize(bases_total = sum(value))
 
 
-gs_files <- list.files('tables/jellyfish_histograms/genomescope/', recursive = TRUE,
+
+
+
+parent_dir <- 'kmer_analysis/genomescope/'
+gs_files <- list.files(parent_dir, 
+                       recursive = TRUE,
                        pattern = 'summary_fix.txt')
 
 gs_summary <- lapply(gs_files, function(x){
-  read_table2(paste0('tables/jellyfish_histograms/genomescope/', x), 
+  read_table2(paste0(parent_dir, x), 
              skip = 3) %>%
     mutate(genome = dirname(x)) %>%
-    select(-X4)
-}) %>% bind_rows() %>%
-  filter(property == 'GenomeHaploidLength')
+    select(-X4) ## strip the empty column
+}) %>% bind_rows()
  
+
+
 gs_summary %>% filter(property == 'ModelFit') %>% View()
 
-data <- left_join(gs_summary, read_stats, by = 'genome') %>%
-  mutate(min = as.numeric(min)) %>%
-  mutate(max = as.numeric(max)) %>%
+data <- gs_summary %>%
+  filter(property == 'GenomeHaploidLength') %>%
+  left_join(., read_coverage, by = 'genome') %>%
   mutate(coverage = bases_total/max)
 
-data %>% filter(coverage >= 25) %>% select(genome, min, max, coverage)
+data %>% filter(coverage >= 25) %>% 
+  select(genome, min, max, coverage) %>%
+  write_csv(., path = 'tables/genome_scope_check.csv')
