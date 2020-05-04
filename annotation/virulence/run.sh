c6ceb6b106509fd683957b3b9c1a712dcb33cd16
Kirsten Gotting <k.gotting@gmail.com>
2020-05-04 15:36:34 -0500

    updating virulence run info

--- annotation/virulence/run.sh
+++ annotation/virulence/run.sh
@@ -1,1 +1,2 @@
-for i in `ls`; do ~/scripts/parseBlast.R $i ../parsed/$i 90; done
+for i in `ls ../../proteins/`; do diamond blastp -d db/allgenes.fa.dmnd -q ../../proteins/$i -o $i; done
+for i in `ls`; do ~/scripts/parseBlast.R $i ../parsed/$i; done
